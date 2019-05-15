fcd92fad3342207a9a320998bc6b0b6ffb7eb906
Caleb J Caldwell <ccaldw2@uwo.ca>
2019-05-15 03:53:08 -0400

    yeet

--- A3.js
+++ A3.js
@@ -1,8 +1,7 @@
 var colors = ["#e51414", "#f9a82d", "#fdef69", "#78f048", "#0954f2", "#abf958", "#816dfd"];
-var randomColor = colorPicker();
 
 function colorPicker(){
-	var r = Math.floor(Math.random() * 7 ) + 1;
+	var r = Math.floor(Math.random() * 7 );
 	return colors[r];
 }
 
@@ -36,41 +35,44 @@ function circle(x, y, r) {
 }
 
 class ball {
-    constructor(x_val = 0,y_val = 0,radius = 10) {
+    constructor(x_val = 0,y_val = 0,radius = 10, color = "#000000") {
         this.x_val = x_val
         this.y_val = y_val
         this.radius = radius
+        this.color = color
     }
     draw(){
         ctx.beginPath();
         ctx.arc(this.x_val, this.y_val, this.radius, 0, 2 * Math.PI);
         ctx.stroke();
-	      ctx.strokeStyle = randomColor;
+        ctx.strokeStyle = this.color;
     }
 }
 
 function bounce() {
-    var t = new Date()/10;
+    var t = Math.floor (new Date()/10)
+    console.log(t%400)
+    if(t%400 <= 390 && t%400 >= 389) {
+        cp = colorPicker()
+    }
+
     ctx.save();
     ctx.clearRect(0, 0, 800, 800);
 
     for (var i = 1; i <= 5; i++) {
         var t_delay = t + i * Math.pow(25,1/2)
 
-        var bounce_state
+        var bounce_state = (1/100) * Math.pow(t_delay%400 -200, 2) + 400
 
-        if (t_delay%200 === t_delay%400){
-            bounce_state = (1/100) * Math.pow(t_delay%200, 2) + 400
-        }
-        else{
-            bounce_state = (1/100) * Math.pow(t_delay%200-200, 2) + 400
-        }
-        new ball(t_delay%800, bounce_state, 5*i).draw()
+        var my_ball = new ball((t_delay-200)%800, bounce_state, 5*i, cp)
+        my_ball.draw()
 
     }
 
     requestAnimationFrame(bounce)
 }
 setUpCanvas(800, 800)
+var cp = '#000'
+
 window.requestAnimationFrame(bounce)
 
